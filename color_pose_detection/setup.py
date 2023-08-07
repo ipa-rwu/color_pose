49be60b17ac44940deb5e28f493d128d573eca55
Vishnuprasad Prachandabhanu <vishnu.pbhat93@gmail.com>
2023-08-07 20:55:37 +0200

    Restructure the pkgs

--- color_pose_detection/setup.py
+++ color_pose_detection/setup.py
@@ -1,20 +1,19 @@
-
+from setuptools import find_packages, setup
 import glob
 import os
-from setuptools import setup
 
-package_name = 'color_pose_estimation'
+package_name = 'color_pose_detection'
 
 setup(
     name=package_name,
     version='0.0.0',
-    packages=[package_name],
+    packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name + '/utils', glob.glob(os.path.join('utils', '*.ply'))),
         ('share/' + package_name, ['package.xml']),
-        ('share/color_pose_estimation/launch',
+        ('share/color_pose_detection/launch',
             glob.glob(os.path.join('launch', '*launch.py'))),
     ],
     install_requires=['setuptools'],
@@ -26,8 +25,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'color_pose_estimation = color_pose_estimation.color_pose_estimation:main',
-            'color_pose_estimation_unique = color_pose_estimation.color_pose_estimation_unique:main',
+            'color_pose_estimation = color_pose_detection.color_pose_estimation:main',
+            'color_pose_estimation_unique = color_pose_detection.color_pose_estimation_unique:main',
         ],
     },
 )
